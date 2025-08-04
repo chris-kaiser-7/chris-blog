c58131900b87b99d3b81c757e43b9f2c74f07714
Christopher Kaiser <58376705+chris-kaiser-7@users.noreply.github.com>
2025-08-03 21:52:56 -0400

    added openshift tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Page() {
 				My Portfolio
 			</h1>
 			<p className="mb-4">
-				{`My name is Christopher Kaiser and this is where I post my work, opinons, and random stuff. I love Go, NeoVim, Kubernetes, and Nextjs amongst other things. I have 6 years of full stack experience and love starting new projects (though I need to get better at finishing them). Feel free to look around, use my code on github, or reach out to me.`}
+				{`My name is Christopher Kaiser and this is where I post my work, opinions, and random stuff. I love Go, NeoVim, Kubernetes, and Nextjs amongst other things. I have 6 years of full-stack experience and love starting new projects (though I need to get better at finishing them). Feel free to look around, use my code on github, or reach out to me.`}
 			</p>
 			<p className="mb-4">
-				{`I am currently open to work. Please reach out to me if you are interested in hiring me`}
+				{`I am currently open to work. Please reach out to me if you are interested in hiring me.`}
 			</p>
 		</section>
 	)
